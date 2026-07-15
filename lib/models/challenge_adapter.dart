3ff37870ed68a859a8d5a38e2ab10e75c871a088
Narciso Terrão Barroso <tbnarciso13@gmail.com>
2026-07-15 02:23:24 +0100

    Fix: Backward-compatible ChallengeAdapter for old stored data
    ChallengeAdapter now handles both old format (category/difficulty/shots)
    and new format (type/sips/giveSips) with automatic mapping of old
    category names to new type names

--- lib/models/challenge_adapter.dart
+++ lib/models/challenge_adapter.dart
@@ -12,14 +12,53 @@ class ChallengeAdapter extends TypeAdapter<Challenge> {
     for (int i = 0; i < numOfFields; i++) {
       fields[reader.readByte()] = reader.read();
     }
+
+    // Handle both old format (category/difficulty/shots) and new format (type/sips/giveSips)
+    final id = fields[0].toString();
+    final type = fields[1].toString(); // old: category, new: type
+    final textPT = fields[2].toString();
+    final textEN = fields[3].toString();
+    final sips = fields[4] is int ? fields[4] as int : int.tryParse(fields[4].toString()) ?? 0;
+    final giveSips = fields[5] is bool ? fields[5] as bool : false;
+    final giveSipsAmount = fields[6] is int ? fields[6] as int : 0;
+
+    // Map old category names to new type names
+    String mappedType = type;
+    switch (type) {
+      case 'bebida':
+        mappedType = 'normal';
+        break;
+      case 'desafio':
+        mappedType = 'normal';
+        break;
+      case 'verdade':
+        mappedType = 'normal';
+        break;
+      case 'seria_ou_beberia':
+        mappedType = 'normal';
+        break;
+      case 'penalidade_max':
+        mappedType = 'bottoms_up';
+        break;
+      case 'regra':
+        mappedType = 'normal';
+        break;
+      case 'grupo':
+        mappedType = 'game';
+        break;
+      case 'coragem':
+        mappedType = 'normal';
+        break;
+    }
+
     return Challenge(
-      id: fields[0] as String,
-      type: fields[1] as String,
-      textPT: fields[2] as String,
-      textEN: fields[3] as String,
-      sips: fields[4] as int,
-      giveSips: fields[5] as bool,
-      giveSipsAmount: fields[6] as int,
+      id: id,
+      type: mappedType,
+      textPT: textPT,
+      textEN: textEN,
+      sips: sips,
+      giveSips: giveSips,
+      giveSipsAmount: giveSipsAmount,
     );
   }
 
